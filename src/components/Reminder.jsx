f64f748214dbc2c19e8544d664cc37d0f6bbbbed
jamsheer adiyantakath <jamshe.jamsheer@gmail.com>
2024-08-02 18:37:41 +0530

    first commit

---
+++ src/components/Reminder.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Reminder.css'
+import { useState } from 'react'
+
+function Reminder() {
+    const [reminder,setReminder] = useState([])
+    const [newReminder,setNewReminder] = useState("")
+
+    const handleInputChange = (event)=>{
+        setNewReminder(event.target.value)
+    }
+
+    const handleAddReminder = ()=>{
+        if(newReminder.trim()){
+            setReminder([...reminder,newReminder])
+            setNewReminder('')
+        }
+    }
+
+    const handleDeleteReminder = (index)=>{
+        setReminder(reminder.filter((item,itemIndex)=> itemIndex != index))
+    }
+
+
+  return (
+    <div className='container'>
+        <h1>Reminder App</h1>
+            <div className="input-container">
+                <input 
+                type="text" 
+                value={newReminder}
+                placeholder='Enter a Reminder'
+                onChange={handleInputChange}
+                />
+                <button className='add-btn' onClick={handleAddReminder}>Add Reminder</button>
+            </div>
+           {
+            reminder.length > 0 ?(
+                <ul className='reminder-list'>
+                {
+                    reminder.map((reminder,index)=>{
+                        return <li key={index}>
+                            {reminder}
+                            <button onClick = {()=>handleDeleteReminder(index)} className='delete-btn'  >Delete</button>
+                        </li>
+
+                    })
+                }
+            </ul>
+            ): <p>No reminders found</p>
+           }
+    </div>
+  )
+}
+
+export default Reminder
